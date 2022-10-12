e727280c69aceabb30316f85608dfb5c354e52ce
Lacedaimon95 <svetozar95@gmail.com>
2022-10-12 17:39:00 +0200

    master/navbar style

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,10 +1,10 @@
-import './App.scss';
-import Navbar from './components/Navbar';
+import "./App.scss";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
     </div>
   );
 }
--- personal-website/src/components/Navbar.js
+++ personal-website/src/components/Navbar.js
@@ -4,34 +4,35 @@ const Navbar = () => {
   const handleMenu = () => {
     let nav = document.querySelector(".navbar");
     nav.classList.toggle("show-menu");
-    console.log(nav);
   };
 
   return (
-    <section className="navbar" onClick={() => handleMenu()}>
-      <div className="navbar__hamburger">
+    <section className="navbar">
+      <div className="navbar__hamburger" onClick={() => handleMenu()}>
         <div className="navbar__hamburger__line one"></div>
         <div className="navbar__hamburger__line two"></div>
         <div className="navbar__hamburger__line three"></div>
       </div>
 
-      <ul className="navbar__list">
-        <li className="navbar__list__item">
-          <a href="#"> Opt1</a>
-        </li>
-        <li className="navbar__list__item">
-          <a href="#"> Opt1</a>
-        </li>
-        <li className="navbar__list__item">
-          <a href="#"> Opt1</a>
-        </li>
-        <li className="navbar__list__item">
-          <a href="#"> Opt1</a>
-        </li>
-        <li className="navbar__list__item">
-          <a href="#"> Opt1</a>
-        </li>
-      </ul>
+      <div className="navbar__box">
+        <ul className="navbar__box__list">
+          <li className="navbar__box__list__item">
+            <a href="#"> Home</a>
+          </li>
+          <li className="navbar__box__list__item">
+            <a href="#"> About me</a>
+          </li>
+          <li className="navbar__box__list__item">
+            <a href="#"> Skills</a>
+          </li>
+          <li className="navbar__box__list__item">
+            <a href="#"> Projects</a>
+          </li>
+          <li className="navbar__box__list__item">
+            <a href="#"> Contact</a>
+          </li>
+        </ul>
+      </div>
     </section>
   );
 };
